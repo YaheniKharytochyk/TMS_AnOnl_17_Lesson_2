2dc8c25bf7184f412d58c96c863db2d57de0a4bb
yauhenikharytonchyk <evgeny200224@gmail.com>
2023-12-20 16:21:40 +0100

    Lesson_1 add to Lesson_2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hi guys");
+        int number = 100;
+        System.out.println(number);
+        int x = 1;
+        int y = 20;
+        int z = 44;
+        System.out.println(x*y-z+x);
 
     }
 }
